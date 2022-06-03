dbae08ef9fb04e91206091b82dc2b83155f56e49
hmacdope <hugomacdermott@gmail.com>
2022-06-03 16:23:55 +1000

    benchify

--- bench.cc
+++ bench.cc
@@ -12,15 +12,15 @@ public:
 
     double result[N * N] = {0};
 
+    // classes that mock atomgroup
+    AGWrapper ag_mock1 = AGWrapper(N);
+    AGWrapper ag_mock2 = AGWrapper(N);
+
+    FloatWrapper float_mock1 = FloatWrapper(N);
+    FloatWrapper float_mock2 = FloatWrapper(N);
+
     void SetUp(const ::benchmark::State &state)
     {
-
-        // classes that mock atomgroup
-        auto ag_mock1 = AGWrapper(N);
-        auto ag_mock2 = AGWrapper(N);
-
-        auto float_mock1 = FloatWrapper(N);
-        auto float_mock2 = FloatWrapper(N);
 
         for (uint64_t i = 0; i < 3 * N; i++)
         {
@@ -39,17 +39,57 @@ public:
         {
             _calc_distance_array(coords1, N, coords2, N, result);
         }
-        state.SetItemsProcessed(N*N * state.iterations());
+        state.SetItemsProcessed(N * N * state.iterations());
         state.counters["Per Result"] = benchmark::Counter(
-        N * state.iterations(),
-        benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+            N * state.iterations(),
+            benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
     }
+
+    void BM_DistanceArrayAg(benchmark::State &state)
+    {
+        for (auto _ : state)
+        {
+            DistanceArray(ag_mock1, ag_mock2, result);
+        }
+        state.SetItemsProcessed(N * N * state.iterations());
+        state.counters["Per Result"] = benchmark::Counter(
+            N * state.iterations(),
+            benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+    }
+
+
+    void BM_DistanceArrayFloatWrap(benchmark::State &state)
+    {
+        for (auto _ : state)
+        {
+            DistanceArray(float_mock1, float_mock2, result);
+        }
+        state.SetItemsProcessed(N * N * state.iterations());
+        state.counters["Per Result"] = benchmark::Counter(
+            N * state.iterations(),
+            benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+    }
+
 };
 
-BENCHMARK_F(Distance, calc_bonds)
+BENCHMARK_F(Distance, distance_array)
 (benchmark::State &state)
 {
     BM_calc_distance_array(state);
+}
+
+
+BENCHMARK_F(Distance, DistanceArrayAg)
+(benchmark::State &state)
+{
+    BM_DistanceArrayAg(state);
+}
+
+
+BENCHMARK_F(Distance, DistanceArrayFloatWrap)
+(benchmark::State &state)
+{
+    BM_DistanceArrayFloatWrap(state);
 }
 
 BENCHMARK_MAIN();
