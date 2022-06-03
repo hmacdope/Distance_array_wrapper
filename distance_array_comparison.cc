bec374918fb1198ec5f169b68233e4ed4dfd7520
hmacdope <hugomacdermott@gmail.com>
2022-06-03 13:48:55 +1000

    working version of Ag-Ag

--- distance_array_comparison.cc
+++ distance_array_comparison.cc
@@ -31,10 +31,10 @@ public:
     int N;
     std::vector<int> ix;
     std::vector<float> coords;
+    int i = 0;
 
-    AtomGroupMock(int N_)
+    AtomGroupMock(int N) : N(N)
     {
-        N = N_;
         for (int i = 0; i < N; i++)
         {
             ix.push_back(static_cast<float>(i));
@@ -44,22 +44,89 @@ public:
             coords.push_back(static_cast<float>(i));
         }
     }
+
+    float *next()
+    {
+        if (i  < N)
+        {
+            i += 1;
+            float *ptr = coords.data() + 3 * (i - 1);
+            return ptr;
+        }
+        else
+        {
+            reset_iteration();
+            return next();
+        }
+    }
+
+    void reset_iteration()
+    {
+        i = 0;
+    }
+};
+
+class FloatWrapper
+{
+
+public:
+    int N;
+    std::vector<float> coords;
+    int i = 0;
+
+    FloatWrapper(int N) : N(N)
+    {
+        for (int i = 0; i < 3 * N; i++)
+        {
+            coords.push_back(static_cast<float>(i));
+        }
+    }
+
+    float *next()
+    {
+        if (i  < N)
+        {
+            i += 1;
+            float *ptr = coords.data() + 3 * (i - 1);
+            return ptr;
+        }
+        else
+        {
+            reset_iteration();
+            return next();
+        }
+    }
+
+    void reset_iteration()
+    {
+        i = 0;
+    }
 };
 
 // wrapper for plain float *
-template<typename T>
-T *_wraps_ag(T* inp)
+template <typename T>
+T *iter_coords(T *inp)
 {
-    return inp;
+    inp += 3;
+    return inp + 3;
 }
 
 // wrapper for AtomGroupMock, MUST BE PASSED BY REF, otherwise new one constructed and
 // ref invalid upon exit
-auto _wraps_ag(AtomGroupMock& inp)
+auto iter_coords(AtomGroupMock &inp)
 {
-     float* pointer =  inp.coords.data();
-     return pointer;
+    float *pointer = inp.next();
+    return pointer;
 }
+
+// wrapper for FloatWrapper, MUST BE PASSED BY REF, otherwise new one constructed and
+// ref invalid upon exit
+auto iter_coords(FloatWrapper &inp)
+{
+    float *pointer = inp.next();
+    return pointer;
+}
+
 
 // directly from MDA
 void _calc_distance_array(float *ref, uint64_t numref, float *conf,
@@ -70,10 +137,9 @@ void _calc_distance_array(float *ref, uint64_t numref, float *conf,
     double rsq;
 
     // avoid dealing with coordinate in type signature
-    // this should be a reinterpret cast so cheap 
-    coordinate* conf_ = (coordinate*) conf;
-    coordinate* ref_ = (coordinate*) ref;
-
+    // this should be a reinterpret cast so cheap
+    coordinate *conf_ = (coordinate *)conf;
+    coordinate *ref_ = (coordinate *)ref;
 
     for (i = 0; i < numref; i++)
     {
@@ -88,48 +154,35 @@ void _calc_distance_array(float *ref, uint64_t numref, float *conf,
     }
 }
 
-
-
-// // overload for IDX.
-// void _calc_distance_array(std::tuple<float*, int*> ref, uint64_t numref, std::tuple<float*, int*> conf,
-//                           uint64_t numconf, double *distances)
-// {
-//     uint64_t i, j;
-//     double dx[3];
-//     double rsq;
-
-//     // avoid dealing with coordinate in type signature
-//     // this should be a reinterpret cast so cheap 
-//     coordinate* conf_ = (coordinate*) conf;
-//     coordinate* ref_ = (coordinate*) ref;
-
-
-//     for (i = 0; i < numref; i++)
-//     {
-//         for (j = 0; j < numconf; j++)
-//         {
-//             dx[0] = conf_[j][0] - ref_[i][0];
-//             dx[1] = conf_[j][1] - ref_[i][1];
-//             dx[2] = conf_[j][2] - ref_[i][2];
-//             rsq = (dx[0] * dx[0]) + (dx[1] * dx[1]) + (dx[2] * dx[2]);
-//             *(distances + i * numconf + j) = sqrt(rsq);
-//         }
-//     }
-// }
-
-template <typename T, typename  U>
+template <typename T, typename U>
 void DistanceArray(T ref, uint64_t numref, U conf, uint64_t numconf, double *distances)
 {
 
-    auto ref_ = _wraps_ag(ref); // float* -> float*
-    auto conf_ = _wraps_ag(conf); // AtomGroupMock -> AtomGroupMock.coords.data (float*)
+    uint64_t i, j;
+    double dx[3];
+    double rsq;
 
-    _calc_distance_array(ref_, numref, conf_, numconf, distances);
+
+    for (i = 0; i < numref; i++)
+    {
+        auto ref__ = iter_coords(ref);
+
+        for (j = 0; j < numconf; j++)
+        {
+            auto conf__ = iter_coords(conf);
+
+            dx[0] = conf__[0] - ref__[0];
+            dx[1] = conf__[1] - ref__[1];
+            dx[2] = conf__[2] - ref__[2];
+            rsq = (dx[0] * dx[0]) + (dx[1] * dx[1]) + (dx[2] * dx[2]);
+            *(distances + i * numconf + j) = sqrt(rsq);
+        }
+    }
 }
 
 int main()
 {
-    // setup 
+    // setup
     constexpr int N = 10;
     constexpr bool debug = false;
     constexpr bool print_result = true;
@@ -145,6 +198,9 @@ int main()
     // classes that mock atomgroup
     auto ag_mock1 = AtomGroupMock(N);
     auto ag_mock2 = AtomGroupMock(N);
+
+    auto float_mock1 = FloatWrapper(N);
+    auto float_mock2 = FloatWrapper(N);
 
     for (int i = 0; i < 3 * N; i++)
     {
@@ -167,11 +223,10 @@ int main()
 
     // wrapped version, float arguments
 
-    DistanceArray(coords1, N, coords2, N, result2);
+    // DistanceArray(coords1, N, coords2, N, result2);
 
-    // wrapped version mixed args
-    DistanceArray(coords1, N, ag_mock1, N, result3);
-
+    // //wrapped version mixed args
+    // DistanceArray(coords1, N, ag_mock1, N, result3);
 
     // wrapped version two Atomgroups
     DistanceArray(ag_mock1, N, ag_mock2, N, result4);
